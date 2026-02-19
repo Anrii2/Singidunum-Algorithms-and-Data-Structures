5541e73b5612af14dc9007c12edc0cffdd11cd3b
Anrii2 <andrijamojovic2@gmail.com>
2026-02-19 20:02:30 +0100

    kraj prvog dana

--- Java/Day1/src/Main.java
+++ Java/Day1/src/Main.java
@@ -1,10 +1,15 @@
+import java.util.Arrays;
+
 public class Main {
+
     public static void main(String[] args) {
+
+        SelectSort select = new SelectSort();
         System.out.println("Hello, World!");
         System.out.println("Ovo je pocetak ucenja Jave po drugi put");
-        int[] arr = {12,34,12,1,53,82,32};
-
-
+        int[] arr = {12, 34, 19, 1, 53, 82, 32};
+        System.out.println(Arrays.toString(arr));
+        select.selectSort(arr);
+        System.out.println(Arrays.toString(arr));
     }
-
 }

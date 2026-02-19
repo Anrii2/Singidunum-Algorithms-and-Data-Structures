95e6a00730ff0584c84cbffb66b62a65700267bc
Anrii2 <andrijamojovic2@gmail.com>
2026-02-19 18:17:56 +0100

    Update Main.java

--- Java/src/Main.java
+++ Java/src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("Ovo je pocetak ucenja Jave po drugi put");
     }
 }
